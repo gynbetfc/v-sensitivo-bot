30ae713ee474563b7e2f1031d127caefc414db58
gynbetfc <gyn.bet.fc@gmail.com>
2026-05-04 22:05:49 -0300

    Corrigir salvamento GitHub + remover 9:30

--- vweb.py
+++ vweb.py
@@ -783,8 +783,7 @@ MAPA_SINAIS = {
     'terceira_igual_primeira': sinal_terceira_igual_primeira,
     'quadrante_de_7': sinal_quadrante_de_7,
     'fluxo_de_velas': sinal_fluxo_de_velas,
-    'nove_e_trinta': sinal_nove_e_trinta,
-    'reversao': sinal_reversao,
+        'reversao': sinal_reversao,
     'm5': sinal_m5
 }
 
