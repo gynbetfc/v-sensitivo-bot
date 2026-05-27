2729642df9c225a495e007ead52aa1a87eb8208d
gynbetfc <gyn.bet.fc@gmail.com>
2026-05-27 12:41:19 -0300

    🔧 Removido imports relativos

--- tesla_369/estrategias/__init__.py
+++ tesla_369/estrategias/__init__.py
@@ -1,19 +1,2 @@
-from .tesla_369 import ESTRATEGIA_TESLA_369
-from .v_sensitivo import ESTRATEGIA_V_SENSITIVO
-from .terceira_igual_primeira import ESTRATEGIA_TERCEIRA
-from .mhi_filtrado import ESTRATEGIA_MHI
-from .quadrante_de_7 import ESTRATEGIA_QUADRANTE
-from .fluxo_de_velas import ESTRATEGIA_FLUXO
-from .reversao import ESTRATEGIA_REVERSAO
-from .m5 import ESTRATEGIA_M5
-
-ESTRATEGIAS = {
-    'tesla_369': ESTRATEGIA_TESLA_369,
-    'v_sensitivo': ESTRATEGIA_V_SENSITIVO,
-    'terceira_igual_primeira': ESTRATEGIA_TERCEIRA,
-    'mhi_filtrado': ESTRATEGIA_MHI,
-    'quadrante_de_7': ESTRATEGIA_QUADRANTE,
-    'fluxo_de_velas': ESTRATEGIA_FLUXO,
-    'reversao': ESTRATEGIA_REVERSAO,
-    'm5': ESTRATEGIA_M5
-}
+# Estratégias carregadas diretamente (launcher junta tudo)
+ESTRATEGIAS = {}  # Será preenchido pelo main.py
