451f5a21d6612504bed584c27bcf4c452c91b0a6
gynbetfc <gyn.bet.fc@gmail.com>
2026-05-27 12:51:17 -0300

    🗑️ Removido

--- tesla_369/core/__init__.py
+++
@@ -1,4 +0,0 @@
-# Core do Tesla 369
-from .firebase import salvar_usuario, carregar_usuario, criar_usuario
-from .indicadores import sma, bollinger, rsi, macd, estocastico
-from .mercado_pago import gerar_pix_mercadopago, verificar_pagamento_mp
