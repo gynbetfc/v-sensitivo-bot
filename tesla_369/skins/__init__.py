830d6101d775ac4b09d98e789d550c1014f9c440
gynbetfc <gyn.bet.fc@gmail.com>
2026-05-27 10:47:39 -0300

    Skins init

---
+++ tesla_369/skins/__init__.py
@@ -0,0 +1,21 @@
+from .padrao import SKIN_PADRAO
+from .dark import SKIN_DARK
+from .neon import SKIN_NEON
+from .bobmarley import SKIN_BOBMARLEY
+from .brasil import SKIN_BRASIL
+from .fire import SKIN_FIRE
+from .ice import SKIN_ICE
+from .sakura import SKIN_SAKURA
+from .sunset import SKIN_SUNSET
+from .ocean import SKIN_OCEAN
+from .matrix import SKIN_MATRIX
+from .thunder import SKIN_THUNDER
+from .magos import SKIN_MAGOS
+from .princesa import SKIN_PRINCESA
+
+SKINS = [
+    SKIN_PADRAO, SKIN_DARK, SKIN_NEON, SKIN_BOBMARLEY,
+    SKIN_BRASIL, SKIN_FIRE, SKIN_ICE, SKIN_SAKURA,
+    SKIN_SUNSET, SKIN_OCEAN, SKIN_MATRIX, SKIN_THUNDER,
+    SKIN_MAGOS, SKIN_PRINCESA
+]
