6c0d4050e9a4f90e688d7148fa24b3fdc2ebfd6b
gynbetfc <gyn.bet.fc@gmail.com>
2026-05-27 11:03:40 -0300

    Estratégia fluxo_de_velas

---
+++ tesla_369/estrategias/fluxo_de_velas.py
@@ -0,0 +1,22 @@
+ESTRATEGIA_FLUXO = {
+    'nome': '🌊 FLUXO-DE-VELAS 🆓', 'desc': '5 velas mesma cor + MM',
+    'timeframe': 60, 'pares': ['EURUSD-OTC'], 'preco_moedas': 0, 'gratis': True
+}
+
+def sinal_fluxo(API, par, timeframe_atual):
+    from datetime import datetime
+    import time
+    if datetime.now().second % 55 != 0: return None, {}
+    v = API.get_candles(par, timeframe_atual, 22, time.time())
+    if len(v) < 22: return None, {}
+    velas = []
+    for vela in v[-5:]:
+        if vela['open'] < vela['close']: velas.append('g')
+        elif vela['open'] > vela['close']: velas.append('r')
+        else: velas.append('d')
+    cores = ''.join(velas)
+    mm = sum(c['close'] for c in v[:-1]) / 21
+    analise = {'preco': v[-1]['close'], 'rsi': None, 'mm5': None, 'mm10': None, 'mm20': round(mm,6), 'stoch': None, 'fase': 'FLUXO'}
+    if v[-1]['close'] > mm and cores == 'ggggg' and 'd' not in cores: return 'call', analise
+    if v[-1]['close'] < mm and cores == 'rrrrr' and 'd' not in cores: return 'put', analise
+    return None, analise
